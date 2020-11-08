e1ec8f3f427167926a86af5f377df6dcbb48a1af
Exopole <alexandre.nicaise28@gmail.com>
2020-11-08 20:13:16 +0100

    Deplacement controller

---
+++ Assets/Script/ControllerPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(DeplacementBehaviour))]
+public class ControllerPlayer : MonoBehaviour
+{
+
+    public DeplacementBehaviour DeplacementBehaviour;
+	public StandaloneInputModule InputModule;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DeplacementBehaviour = GetComponent<DeplacementBehaviour>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Debug.Log(InputModule.input.GetAxisRaw(InputModule.horizontalAxis));
+
+        Deplacement();
+        
+    }
+
+    private void Deplacement()
+	{
+		Vector3 direction = new Vector3();
+        direction.x = InputModule.input.GetAxisRaw(InputModule.horizontalAxis);
+        direction.z = InputModule.input.GetAxisRaw(InputModule.verticalAxis);
+        DeplacementBehaviour.Direction = direction.normalized;
+	}
+}
--- Assets/Script/DeplacementBehaviour.cs
+++ Assets/Script/DeplacementBehaviour.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(CharacterController))]
 public class DeplacementBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private CharacterController mCharacterController;
+	public Vector3 Direction;
+
+	public float Speed;
+
+	private void Awake()
+	{
+		mCharacterController = GetComponent<CharacterController>();
+	}
+
+	private void LateUpdate()
+	{
+		mCharacterController.Move(Direction * Time.deltaTime * Speed);
+	}
+
+
 }
